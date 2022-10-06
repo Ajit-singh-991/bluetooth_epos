c7244f8e4f585d06c7718c692c91ee5311bb3f6c
Ajit-singh-991 <rathoreajit991@gmail.com>
2022-10-06 23:55:36 +0530

    new bluetooth package

--- lib/bluetooth_epos.dart
+++ lib/bluetooth_epos.dart
@@ -1,7 +1,8 @@
 library bluetooth_epos;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+
+
+export './src/bluetooth_manager.dart';
+export './src/bluetooth_device.dart';
+
+
